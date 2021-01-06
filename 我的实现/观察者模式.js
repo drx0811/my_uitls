9c4cc33c5e963900372e2013008f593d3813ffe4
drx0811 <969697205@qq.com>
2021-01-06 22:37:01 +0800

    556

---
+++ 我的实现/观察者模式.js
@@ -0,0 +1,26 @@
+class Envent{
+  constructor() {
+    this.arr={};
+  }
+  on(type,fn){
+    if(!this.arr[type]){
+      this.arr[type]=[];
+    }
+    this.arr[type].push(fn);
+  }
+  emit(type,...args){
+    if(!this.arr[type]){
+      throw '无该订阅者'
+    }else{
+      this.arr[type].forEach(item=>{
+        item.call(this,args)
+      })
+    }
+  }
+}
+let envent=new Envent();
+envent.on('data',function(args) {
+  console.log("调用了data",args);
+});
+envent.emit('data',"11","22");
+envent.emit('data',"111","222");
