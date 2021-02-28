4c166a783752e9326ad8e8924e5d1f71b46c4b99
drx0811 <969697205@qq.com>
2021-02-28 13:00:03 +0800

    feat:useEffect

---
+++ 我的实现/react-hooks/src/UseEffect.js
@@ -0,0 +1,30 @@
+
+import React from 'react';
+
+let _depArray;
+function UseEffect(callback,deps) {
+  let noDeps=!deps;
+  let hasDepsChange= _depArray?!deps.every((el,i)=>el===_depArray[i]):true;
+  if(noDeps || hasDepsChange){
+    callback();
+    _depArray=deps;
+  }
+}
+
+function UseEffectFn(props) {
+  const {RenderFn}=props;
+  UseEffect(()=>{
+    console.log('11111 :>> ', 11111);
+  },[])
+
+  
+  return (
+    <div>
+    1112222
+    </div>
+  );
+}
+export default UseEffectFn;
+
+
+
--- 我的实现/react-hooks/src/index.js
+++ 我的实现/react-hooks/src/index.js
@@ -2,9 +2,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import UseStateFn from './useState'
+import UseEffectFn from './UseEffect'
 function RenderFn() {
   ReactDOM.render(
-    <UseStateFn RenderFn={RenderFn} />,
+    // <UseStateFn RenderFn={RenderFn} />,
+    <UseEffectFn RenderFn={RenderFn} />,
     document.getElementById('root')
   );
 }
