2986d7746a9ff68346f2bfe02b10fdd5a028576a
drx0811 <969697205@qq.com>
2021-03-17 23:40:56 +0800

    feat:node 事件环

--- kownlenge/浏览器和node事件循环.js
+++ kownlenge/浏览器和node事件循环.js
@@ -1,3 +1,6 @@
+
+// 宏任务：一般都是宿主环境提供的api方法；比如script，定时器，ui渲染 setImmediate  ；
+// 微任务：一般是由语言提供的api方法；比如 promise的then方法，mutationObserver process.nextTick()
 /*
 * 先执行script宏任务加载，遇到微任务后会放到 栈 中，（后进先出）；
 * 执行完本轮事件的最后一个微任务后进行渲染；渲染完毕之后  代表本轮事件执行完毕
@@ -9,3 +12,32 @@
 * 4. 一轮 微任务中 只会执行最后存在的dom渲染；
 *
 * */
+
+// node 的setImmediate 是个宏任务；在poll阶段如果有发现有setImmediate 那么会立即进入check阶段，否则会进入下一轮
+// 也就是 poll（I/O）---> setImmediate----> setTimeout
+fs.readFile('...',(err,data)=>{
+  if (err) {
+    throw new Error('发生了错误！')
+  } else {
+    setTimeout(()=>{
+      console.log(1111);
+    },0)
+    setImmediate(()=>{
+      console.log(2222);
+    })
+  }
+})
+// 2222 ------> 1111
+
+// 在node中的 process.nextTick() 比 Promise 执行的优先级更高；
+setTimeout(()=>{
+  Promise.resolve().then(res=>{
+    console.log(1111);
+  })
+  process.nextTick(()=>{
+    console.log(2222);
+  })
+})
+// 2222----->11111
+
+
