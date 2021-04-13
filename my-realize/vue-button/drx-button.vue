fca694df223a8b73be74b0cc6ec276ed9d2a9d24
drx0811 <969697205@qq.com>
2021-04-13 21:52:11 +0800

    feat:vue-button

---
+++ my-realize/vue-button/drx-button.vue
@@ -0,0 +1,23 @@
+<template>
+  <div @click="drxClick($event)" :class="['drx-button-box',disabled!==true?'drx-button-box-disabled':'']">
+    <img class="drx-icon" :src="icon" alt="">
+    <span class="drx-text" v-if="this.$slots.default">
+      <slot>按钮</slot>
+    </span>
+  </div>
+</template>
+<script>
+  export default {
+    name:'drx-button',
+    props:[
+      'disabled',
+      'icon',
+      'type'
+    ],
+    methods:{
+      drxClick(e){
+        this.$listeners.click(e)
+      }
+    }
+  }
+</script>
---
+++ my-realize/vue-button/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <drx-button @click="show($event)" icon="https://img01.yzcdn.cn/vant/ipad.jpeg" disabled  type='primary'>
+    测试button
+  </drx-button>
+</template>
+<script>
+  export default {
+    name:'drx',
+    data(){
+
+    },
+    methods:{
+      show(e){
+        console.log(e);
+      }
+    }
+  }
+</script>
