a0f76f703cad9830f8ed430e685dc0fbc0d1f46f
drx0811 <969697205@qq.com>
2021-05-20 21:41:18 +0800

    feat:测试ts

--- kownlenge/typeScript/自定义.ts
+++ kownlenge/typeScript/自定义.ts
@@ -1,49 +1,49 @@
 /**
  * 类型别名的作用 type
  */
-type IUserInfo={
-  name:string;
-  age:number;
-  sex:boolean;
-  info:{
-    height:number;
-    address:string;
+type IUserInfo = {
+  name: string;
+  age: number;
+  sex: boolean;
+  info: {
+    height: number;
+    address: string;
   }
 }
-const userInfo:IUserInfo={
-  name:"drx",
-  age:30,
-  sex:true,
-  info:{
-    height:188,
-    address:'qh'
+const userInfo: IUserInfo = {
+  name: "drx",
+  age: 30,
+  sex: true,
+  info: {
+    height: 188,
+    address: 'qh'
   }
 }
 // 实现partial;
-type MyPartial<T>={
-  [U in keyof T]?:T[U]
+type MyPartial<T> = {
+  [U in keyof T]?: T[U]
 }
 // 重新定义一个 实际使用的类型别名;
-type IUserInfoMy=MyPartial<IUserInfo>;
-const drxUserInfo:IUserInfoMy={
-  name:'drx',
-  age:30,
-  sex:true
+type IUserInfoMy = MyPartial<IUserInfo>;
+const drxUserInfo: IUserInfoMy = {
+  name: 'drx',
+  age: 30,
+  sex: true
 }
 
 // 实现多层的 DeepPartial
-type MyDeepPartial<U>={
-  [T in keyof U]?:U[T] extends Object ?MyDeepPartial<U[T]>:U[T]
+type MyDeepPartial<U> = {
+  [T in keyof U]?: U[T] extends Object ? MyDeepPartial<U[T]> : U[T]
 }
 
 // 重新顶一个 实际使用的类型别名;
-type IUserInfoMyDeep=MyDeepPartial<IUserInfo>
-const drxUserInfoMyDeep:IUserInfoMyDeep={
-  name:"drx",
-  age:12,
-  sex:false,
-  info:{
-    height:120
+type IUserInfoMyDeep = MyDeepPartial<IUserInfo>
+const drxUserInfoMyDeep: IUserInfoMyDeep = {
+  name: "drx",
+  age: 12,
+  sex: false,
+  info: {
+    height: 120
   }
 }
 
@@ -51,12 +51,12 @@ const drxUserInfoMyDeep:IUserInfoMyDeep={
  * typeof 的作用 可以把一个对象的类型来作为接口来使用
  */
 
-type IMyTypeOf=typeof drxUserInfoMyDeep;
-const MyTypeOf:IMyTypeOf={
-  name:'drx',
-  age:12,
-  info:{
-    height:12
+type IMyTypeOf = typeof drxUserInfoMyDeep;
+const MyTypeOf: IMyTypeOf = {
+  name: 'drx',
+  age: 12,
+  info: {
+    height: 12
   }
 }
 
@@ -69,45 +69,55 @@ const MyTypeOf:IMyTypeOf={
  *    必选的,那么Required是必用之工具;
  * )
  */
-type INotImportant={
-  name?:'drx',
-  age:number;
-  info?:{
-    height:number;
+type INotImportant = {
+  name?: 'drx',
+  age: number;
+  info?: {
+    height: number;
   }
 }
 
 // 适用Required
-type IImportant=Required<INotImportant>;
-const importantInfo:INotImportant={
-  name:'drx',
-  age:10,
-  info:{
-    height:1920
+type IImportant = Required<INotImportant>;
+const importantInfo: INotImportant = {
+  name: 'drx',
+  age: 10,
+  info: {
+    height: 1920
   }
 }
 
 
 // 封装自己的 Required
-type MuRequired<U>={
-  [T in keyof U]-?:U[T]
+type MuRequired<U> = {
+  [T in keyof U]-?: U[T]
 }
-type MyImportant=MuRequired<INotImportant>;
-const myImportantInfo:MyImportant={
-  name:'drx',
-  age:10,
-  info:{
-    height:30
+type MyImportant = MuRequired<INotImportant>;
+const myImportantInfo: MyImportant = {
+  name: 'drx',
+  age: 10,
+  info: {
+    height: 30
   }
 }
 
 
+interface Seal {
+  name: string;
+  url: string;
+}
+interface API {
+  "/user": { name: string; age: number; phone: string };
+  "/seals": { seal: Seal[] };
+}
 
 
 
+const api=<URL extends keyof API>(url):Promise<API[URL]>=>{
+  return fetch(url).then(res=>res.json())
+}
 
-
-
+api('/seals').then(res=>[{name:'1',url:'111'}])
 
 
 
